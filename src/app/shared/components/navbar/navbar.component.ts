c6ede6d3b11d634e3a0bbde2aad2b6efcbe07520
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2025-03-14 08:21:18 +0100

    support for mobile navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
 import { AuthService, User } from '../../../services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -13,12 +15,17 @@ import { AuthService, User } from '../../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
   user: User | null = null;
-
-  constructor(private authService: AuthService) {}
+  isMobileMenuOpen = false;
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.authService.getCurrentUser().subscribe(user => {
       this.user = user;
+    });
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isMobileMenuOpen = false;
     });
   }
 
@@ -28,5 +35,17 @@ export class NavbarComponent implements OnInit {
 
   isLandingPage(): boolean {
     return window.location.pathname === '/welcome';
+  }
+
+  // // isActive(route: string): boolean {
+  // //   return this.router.url === route || this.router.url.startsWith(route + '/');
+  // // }
+  
+  // isLandingPage(): boolean {
+  //   return this.router.url === '/landing' || this.router.url === '/auth/login' || this.router.url === '/auth/register';
+  // }
+  
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
